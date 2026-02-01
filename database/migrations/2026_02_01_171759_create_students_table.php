e472719b92ef9a49d0dd2e763aaf35100d796e05
OMouloud <mouloudouldali@yahoo.fr>
2026-02-01 12:49:15 -0500

    Added student and city models, and created student and city tables

---
+++ database/migrations/2026_02_01_171759_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->date('birth_date')->nullable();
+            $table->unsignedBigInteger('town_id');
+            // $table->foreignId('town_id');
+            $table->timestamps();
+            $table->foreign('town_id')->references('id')->on('towns')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
